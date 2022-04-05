cd7fd871cb203f4c3bf263883a22c4aa469a6f5c
unknown <vrushtipatel007@gmail.com>
2022-04-04 23:08:20 -0400

    [done] Project phrase 2 completed

--- javascript/favourites.js
+++ javascript/favourites.js
@@ -1,5 +1,10 @@
 let url = '/assets/images/';
 
+// Automatically load the page
+window.onload = function () {
+    displayItems();
+}
+
 // create array of objects for items
 let itemList = [{
         imageUrl: url + 'panipuri.jpg',
@@ -51,26 +56,35 @@ let itemList = [{
     }
 ];
 
-let favItemData = '';
+function displayItems() {
+    let favItemData = '';
 
-// display item details using for loop and html attributes
-for (let item of itemList) {
-    favItemData += '<div class="item">';
-    favItemData += '<div class="card">';
-    favItemData += `<img class="card-img-top" src=${item.imageUrl} alt="Card image cap">`;
-    favItemData += '<div class="card-body">';
-    favItemData += '<div class="row">';
-    favItemData += '<div class="col-lg-10">';
-    favItemData += `<h5 class="card-title">${item.itemName}</h5>`;
-    favItemData += `<p class="price">Price: <span style="font-weight: 650;">$${item.price}</span></p>`;
-    favItemData += '</div>';
-    favItemData += '<div class="col-lg-2"><i class="fas fa-heart"></i></div>';
-    favItemData += '</div>';
-    favItemData += '<a href="#" class="btn btn-secondary">Add to Cart</a>';
-    favItemData += ' </div>';
-    favItemData += ' </div>';
-    favItemData += ' </div>';
+    // display item details using for loop and html attributes
+    for (let [index, item] of itemList.entries()) {
+        favItemData += '<div class="item">';
+        favItemData += '<div class="card">';
+        favItemData += `<img class="card-img-top" src=${item.imageUrl} alt="Card image cap">`;
+        favItemData += '<div class="card-body">';
+        favItemData += '<div class="row">';
+        favItemData += '<div class="col-lg-10">';
+        favItemData += `<h5 class="card-title">${item.itemName}</h5>`;
+        favItemData += `<p class="price">Price: <span style="font-weight: 650;">$${item.price}</span></p>`;
+        favItemData += '</div>';
+        favItemData += `<div class="col-lg-2"><i class="fas fa-heart" onclick="unFavourite(${index})"></i></div>`;
+        favItemData += '</div>';
+        favItemData += '<a href="#" class="btn btn-secondary">Add to Cart</a>';
+        favItemData += ' </div>';
+        favItemData += ' </div>';
+        favItemData += ' </div>';
+
+        // set html content to the item container for showing list of data
+        document.querySelector('.item-container').innerHTML = favItemData;
+    }
+
 }
 
-// set html content to the item container for showing list of data
-document.querySelector('.item-container').innerHTML = favItemData;
+// function for unfavourite item from item list
+function unFavourite(index) {
+    itemList.splice(index, 1);
+    displayItems();
+}
